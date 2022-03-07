7d2d0828b3401179461cce7bd924ecabf4da4da1
gcxfd <i@rmw.link>
2022-03-07 12:11:19 +0800

    🔶

--- misc/svgo.config.cjs
+++ misc/svgo.config.cjs
@@ -2,11 +2,7 @@ module.exports = {
   plugins: [{
     name: 'preset-default',
     params: {
-      floatPrecision:4,
       overrides: {
-        cleanupNumericValues: {
-          floatPrecision: 4,
-        },
         removeViewBox:false,
       },
     },
